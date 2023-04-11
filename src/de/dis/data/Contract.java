16a19fae75f53f22b7ebe2bd017d20bb3574cd70
clara <clara.vonbargen@hamburg.de>
2023-04-11 17:36:57 +0200

    implemented setValues and getDBFields in Contract and its subclasses analogously to Estate classes

--- src/de/dis/data/Contract.java
+++ src/de/dis/data/Contract.java
@@ -4,8 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Contract {
+    public static final String PLACE = "place";
+    public static final String DATE = "date";
+    public static final String CONTRACT_NUMBER = "contract_number";
     private int contractNumber;
     private Date date;
     private String place;
@@ -44,14 +49,25 @@ public class Contract {
     public void save() {
         try {
             Connection con = DbConnectionManager.getInstance().getConnection();
-            PreparedStatement stmt = con.prepareStatement("INSERT INTO contracts (contract_number, date, place) VALUES (?, ?, ?)");
-            stmt.setInt(1, contractNumber);
-            stmt.setDate(2, new java.sql.Date(date.getTime()));
-            stmt.setString(3, place);
-            stmt.executeUpdate();
-    } catch (SQLException ex) {
-        ex.printStackTrace();
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO contracts (" + getDBFields() + ") VALUES ("
+                    + getDBFields().stream().map(s -> "?").collect(Collectors.joining(", ")) + ") RETURNING id");
+            setValues(stmt);
+            stmt.executeQuery();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    protected void setValues(PreparedStatement stmt) throws SQLException {
+        List<String> columns = getDBFields();
+        stmt.setInt(columns.indexOf(CONTRACT_NUMBER) + 1, contractNumber);
+        stmt.setDate(columns.indexOf(DATE) + 1, new java.sql.Date(date.getTime()));
+        stmt.setString(columns.indexOf(PLACE) + 1, place);
+    }
+
+    public List<String> getDBFields() {
+        return List.of(CONTRACT_NUMBER, DATE, PLACE);
     }
 }
-}
+
 
