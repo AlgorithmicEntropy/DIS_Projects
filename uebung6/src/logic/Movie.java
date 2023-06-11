81d3dbda2fa08b96278e0a0de102cc2aeca05b20
ClaraDvB <clara@kawzeg.com>
2023-06-11 15:21:42 +0200

    implemented TODOs

---
+++ uebung6/src/logic/Movie.java
@@ -0,0 +1,19 @@
+package logic;
+
+/**
+ * Class to hold constants for movies.
+ */
+public class Movie {
+    public static final String COL_ID = "_id";
+    public static final String COL_GENRE = "genre";
+    public static final String COL_RATING = "rating";
+    public static final String COL_TITLE = "title";
+    public static final String COL_VOTES = "votes";
+    public static final String COL_COMMENT = "comment";
+    public static final String COL_TWEETS = "tweets";
+    public static final String COL_TWEETS_TEXT = COL_TWEETS + "." + "text";
+
+    private Movie() {
+
+    }
+}
---
+++ uebung6/src/logic/Tweet.java
@@ -0,0 +1,15 @@
+package logic;
+
+/**
+ * Class to hold constants for tweets.
+ */
+public class Tweet {
+
+    public static final String COL_ID = "_id";
+    public static final String COL_TEXT = "text";
+    public static final String COL_USERNAME = "user.name";
+    public static final String COL_GEOTAGGED = "coordinates";
+
+    private Tweet() {
+    }
+}
